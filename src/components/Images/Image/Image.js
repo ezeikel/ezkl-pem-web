f2e0a30f13405b5d5f2b0bfcfbf06e5caffa08db
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-03 01:27:58 +0000

    Added data-id to all images which is equal to node stored in firebase.

--- src/components/Images/Image/Image.js
+++ src/components/Images/Image/Image.js
@@ -7,7 +7,7 @@ const Image = (props) => {
     return (
       <li className="image">
         <h3>{details.title}</h3>
-        <img src={details.url} alt={details.title} />
+        <img src={details.url} alt={details.filename} data-id={index} />
         <button onClick={() => addLike(index)}>Like</button>
         <h1>{details.likes}</h1>
       </li>
